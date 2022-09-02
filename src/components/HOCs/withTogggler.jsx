03cf9a0d881184a0896c5f9591b973881bcc58ac
enjeytee <enjeytee@gmail.com>
2022-09-02 19:17:11 +0800

    af

---
+++ src/components/HOCs/withTogggler.jsx
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+
+class Toggler extends Component {
+    state = {
+        on: this.props.defaultOnValue
+    }
+    toggle = () => {
+        this.setState(prevState => {
+            return {
+                on: !prevState.on
+            }
+        })
+    }
+    render() {
+        const {component: C, defaultOnValue, ...props} = this.props
+        return (
+            <C 
+                on={this.state.on}
+                toggle={this.toggle}
+                {...props}
+            />
+        )
+    }
+}
+
+export function withToggler(component, optionsObj) {
+    return function(props) {
+        return (
+            <Toggler 
+                component={component}
+                defaultOnValue={optionsObj.defaultOnValue}
+                {...props}
+            />
+        )
+    }
+}
